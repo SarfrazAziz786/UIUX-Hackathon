885984e0b41ca27d230ab15dc14df0a5f6d8c5a3
SarfrazAziz786 <162900723+SarfrazAziz786@users.noreply.github.com>
2024-12-08 21:35:55 +0500

    update page.tsx

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,7 +2,6 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import BrowseTheRange from "./components/BrowseTheRange";
 import React from 'react'
-import OurProducts from "./components/our-products/OurProducts";
 
 
 const Homepage = () => {
